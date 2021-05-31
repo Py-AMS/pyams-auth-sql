65fb92caa3dd7870fd3e0a2514c6f0dcbb1ad1f8
Thierry Florac <thierry.florac@onf.fr>
2021-05-31 22:59:37 +0200

    Updated add menus registration

--- src/pyams_auth_sql/zmi/__init__.py
+++ src/pyams_auth_sql/zmi/__init__.py
@@ -21,7 +21,7 @@ from pyams_layer.interfaces import IPyAMSLayer
 from pyams_pagelet.pagelet import pagelet_config
 from pyams_security.interfaces import ISecurityManager
 from pyams_security.interfaces.base import MANAGE_SECURITY_PERMISSION
-from pyams_security_views.zmi import ISecurityManagerView
+from pyams_security_views.zmi import SecurityPluginsTable
 from pyams_security_views.zmi.plugin import SecurityPluginAddForm, SecurityPluginAddMenu, \
     SecurityPluginPropertiesEditForm
 from pyams_site.interfaces import ISiteRoot
@@ -42,7 +42,7 @@ from pyams_auth_sql import _  # pylint: disable=ungrouped-imports
 
 
 @viewlet_config(name='add-sql-auth-plugin.menu',
-                context=ISiteRoot, layer=IAdminLayer, view=ISecurityManagerView,
+                context=ISiteRoot, layer=IAdminLayer, view=SecurityPluginsTable,
                 manager=IContextAddingsViewletManager, weight=70,
                 permission=MANAGE_SECURITY_PERMISSION)
 class SQLAuthPluginAddMenu(SecurityPluginAddMenu):
