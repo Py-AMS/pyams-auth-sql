85e9092f0ab0190757988f94eded769dce30bd53
Thierry Florac <thierry.florac@onf.fr>
2021-10-13 16:29:48 +0200

    Version 1.1.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ DOCS = os.path.join(os.path.dirname(__file__),
 README = os.path.join(DOCS, 'README.txt')
 HISTORY = os.path.join(DOCS, 'HISTORY.txt')
 
-version = '1.1.1'
+version = '1.1.2'
 long_description = open(README).read() + '\n\n' + open(HISTORY).read()
 
 tests_require = [
@@ -65,7 +65,7 @@ setup(name='pyams_auth_sql',
           # -*- Extra requirements: -*-
           'persistent',
           'pyams_alchemy',
-          'pyams_security',
+          'pyams_security >= 1.8.1',
           'pyams_site',
           'pyams_utils',
           'pyramid',
